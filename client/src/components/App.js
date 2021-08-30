c3266fb3fdb99bf71abbb1df53f77c3556ffb5fe
legendhimslef <sabdulrahman971@gmail.com>
2021-08-30 21:47:29 +0530

    Front End Push

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Header
+      </header>
+      <div>
+        Body
+      </div>
+    </div>
+  );
+}
+
+export default App;
